93cfd8cc9c0493f9d63881dbc8c45698913f3964
Kevin Gillispie <27305807+kevingillispie@users.noreply.github.com>
2021-07-22 16:34:28 -0700

    Update calculator-v.2.js
    Developing the three "registries". Exploring the use of scientific notation.

--- js/calculator-v.2.js
+++ js/calculator-v.2.js
@@ -1,123 +1,170 @@
 /**
- * @param {_X_} float Input register
- * @param {_Y_} float Process register
- * @param {_Z_} float Cumulative register
- * @param {_M_} float Memory register for storage
+ * @param {_X_} object  Input register
+ * @param {_Y_} object  Process register
+ * @param {_Z_} object  Cumulative register
+ * @param {_M_} float   Memory register for storage
  */
-var _X_ = parseFloat(0),
-    _Y_ = parseFloat(0),
-    _Z_ = parseFloat(0),
-    _M_ = parseFloat(0);
+var _X_ = {
+        "x": "",
+        "y": "",
+        "z": "",
+        "operation": ""
+    },
+    _Y_ = {
+        "x": "",
+        "y": "",
+        "operation": ""
+    },
+    _Z_ = {
+        "x": "",
+        "z": "",
+        "operation": ""
+    },
+    _M_ = "";
 /***/
 var re = document.getElementById("result_electronics"),
     powerSwitch = document.querySelector("[data-value=\"power\"]"),
-    radDegSwitch = document.querySelector("[data-value=\"rad-deg\"]");
+    radDegSwitch = document.querySelector("[data-value=\"rad-deg\"]"),
+    userDisplay = document.getElementById("digits_container"),
+    buttons = document.querySelectorAll(".btn");
 const BUTTON_EVENT_FUNCTIONS = {
     "add": function() {
-        depressOperator("add");
+        setOperand("add");
     },
     "arc": function() {
-        depressOperator("arc");
+        setOperand("arc");
     },
     "cos": function() {
-        updateDisplayValue(Math.cos(displayValue), true);
+        console.log(Math.cos(_X_["x"]), true);
     },
     "clear": function() {
-        clear(true);
+        // clear(true);
     },
     "clear-entry": function() {
-        clear(false);
+        _X_["x"] = "";
     },
     "d-r": function() {},
     "decimal": function() {
-        if (displayValue.indexOf(".") == -1) {
-            updateDisplayValue(this.dataset.value, false);
+        if (_X_["x"].indexOf(".") == -1) {
+            console.log(this.dataset.value, false);
         } else {
-            displayFlash();
+            // displayFlash();
         }
     },
     "digit": function() {
-        updateDisplayValue(this.dataset.value, false);
+        inputValue(this.dataset.value);
     },
     "divide": function() {
-        depressOperator("divide");
+        setOperand("divide");
     },
     "ee": function() {},
     "equals": function() {
-        getResult();
+        // getResult();
     },
     "ex": function() {
-        updateDisplayValue(Math.pow(Math.E, displayValue), true);
+        console.log(Math.pow(Math.E, _X_["x"]), true);
     },
     "exchange": function() {
-        exchange(_X_, _Y_);
+        if (_Y_["y"]) {
+            let y = _Y_["y"];
+            _Y_["y"] = _X_["x"];
+            _X_["x"] = y;
+        }
     },
     "factorial": function() {
-        updateDisplayValue(factorial(Math.trunc(displayValue)), true);
+        console.log(factorial(Math.trunc(_X_["x"])), true);
     },
     "hyp": function() {
-        depressOperator("hyperbolic");
+        setOperand("hyperbolic");
     },
     "multiply": function() {
-        depressOperator("multiply");
+        setOperand("multiply");
     },
     "lnx": function() {
-        updateDisplayValue(Math.log(displayValue), true);
+        console.log(Math.log(_X_["x"]), true);
     },
     "log": function() {
-        updateDisplayValue(Math.log10(displayValue), true);
+        console.log(Math.log10(_X_["x"]), true);
     },
     "pi": function() {
-        updateDisplayValue(Math.PI.toPrecision(10), true);
+        console.log(Math.PI.toPrecision(10), true);
     },
     "pos-neg": function() {
-        displayValue = displayValue * -1;
-        updateTerm(displayValue);
-        updateDisplayValue(displayValue, true);
+        _X_["x"] = _X_["x"] * -1;
+        // updateTerm(_X_["x"]);
+        console.log(_X_["x"], true);
     },
     "recall": function() {
-        updateDisplayValue(_M_, true);
-        clearTerms();
+        console.log(_M_, true);
+        // clearTerms();
     },
     "reciprocal": function() {
-        updateDisplayValue(1 / displayValue, true);
+        console.log(1 / _X_["x"], true);
     },
     "sin": function() {
-        updateDisplayValue(Math.sin(displayValue), true);
+        console.log(Math.sin(_X_["x"]), true);
     },
     "sqrt": function() {
-        updateDisplayValue(Math.sqrt(displayValue), true);
+        console.log(Math.sqrt(_X_["x"]), true);
     },
     "squared": function() {
-        updateDisplayValue(Math.pow(displayValue, 2), true);
+        console.log(Math.pow(_X_["x"], 2), true);
     },
     "store": function() {
-        _M_ = (displayValue) ? displayValue : 0;
+        (_X_) ? set_M_(_X_): set_M_("");
         displayFlash();
     },
     "subtract": function() {
-        depressOperator("subtract");
+        setOperand("subtract");
     },
     "sum": function() {
-        _M_ = new Operations(displayValue, ((!_M_) ? 0 : _M_))["add"]();
-        updateDisplayValue(_M_, true);
-        clearTerms();
-        toggleNewTerm();
-        updateNewTermDisplayValue();
+        set_M_(_X_["x"] + _M_);
+        console.log(_M_, true);
+        // clearTerms();
+        // toggleNewTerm();
+        // updateNewTerm_X_();
     },
     "tan": function() {
-        updateDisplayValue(Math.tan(displayValue), true);
+        console.log(Math.tan(_X_["x"]), true);
     },
     "xpower": function() {
-        depressOperator("xPower");
+        setOperand("xPower");
     },
     "xroot": function() {
-        depressOperator("xRoot");
+        setOperand("xRoot");
     }
 };
 /**
- * ONBOOT
+ * HELPER FUNCTIONs
  */
+function getSign(v) {
+    return Math.sign(parseFloat(v));
+}
+
+function set_M_(value = "") {
+    _M_ = parseFloat(value);
+}
+
+function getValueStringLength() {
+    return _X_["x"].length;
+}
+
+function scientificNotation(v) {
+    console.log("Length:", v.length);
+    console.log("X:", _X_["x"]);
+    if (v > 10**10) {
+        console.log("EE9:", parseFloat(v).toExponential(9));
+        console.log("P9", parseFloat(v).toPrecision(10));
+    }
+}
+/**
+ * 
+ */
+
+/**
+ * ONBOOT LOGIC
+ */
+/* CREATE DISPLAY BACKGROUND */
 for (let i = 0; i < 14; i++) {
     re.insertAdjacentHTML('afterbegin', `
         <div class="digit-bg">
@@ -152,3 +199,84 @@ radDegSwitch.addEventListener("click", function() {
 /**
  * /END
  */
+
+/**
+ * POWER ON / OFF
+ */
+function powerOn() {
+    setTimeout(powerFlash, 80);
+    setTimeout(() => {
+        // resetDisplay(10, false, true);
+    }, 100);
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", BUTTON_EVENT_FUNCTIONS[btn.getAttribute("name")]);
+        btn.addEventListener("click", BUTTON_EVENT_FUNCTIONS[(btn.classList.contains("digit") ? "digit" : "")]);
+    });
+}
+
+function powerOff() {
+    // clearFlashingNines();
+    powerFlash();
+    setTimeout(() => {
+        // clearUserDisplay();
+        // clearTerms();
+        set_M_("");
+    }, 25);
+    buttons.forEach((btn) => {
+        btn.removeEventListener("click", BUTTON_EVENT_FUNCTIONS[btn.getAttribute("name")]);
+        btn.removeEventListener("click", BUTTON_EVENT_FUNCTIONS.digit);
+    });
+}
+
+function powerFlash() {
+    // clearUserDisplay();
+    // reset_X_Array(1, false);
+    // printResetDisplay();
+}
+/**
+ * /END
+ */
+
+/**
+ * DISPLAY LOGIC
+ */
+function digitDisplayElement(value = "") {
+    let DIV = document.createElement("DIV");
+    DIV.setAttribute("class", "result");
+    DIV.dataset.value = value;
+    DIV.innerText = value;
+    return DIV;
+}
+
+function formatValueForDisplay(v) {
+
+}
+
+function printValueToDisplay(v) {
+    let sign = (getSign(v) > -1) ? "+" : "-";
+    for (let i = 0; i < 14; i++) {
+        console.log(digitDisplayElement(v));
+    }
+}
+/**
+ * 
+ */
+
+/**
+ * DATA & FUNCTION ENTRY LOGIC
+ */
+
+function inputValue(v) {
+    _X_["x"] += v;
+    scientificNotation(_X_["x"]);
+}
+
+function setOperand(o) {
+    _X_["operation"] = o;
+    if (o === "add" || o === "subtract") {
+        _Z_["operation"] = o;
+    } else {
+        _Y_["operation"] = o;
+    }
+    console.log(_X_["operation"], _Y_["operation"], _Z_["operation"]);
+}
